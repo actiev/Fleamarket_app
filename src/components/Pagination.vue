1523374263f04e8abe9e818919406a5cdb939d54
actiev <viktor.kabashnuy@gmail.com>
2018-05-07 14:35:20 +0300

    edit front pagination

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,17 +1,48 @@
 <template>
-    <div class="pagination">
-        <span class="prev">«</span>
-        <div class="pages">
-            <span class="active">1</span>
-            <span>2</span>
-            <span>3</span>
+    <div class="pagination" @click="changePage">
+        <span class="prev" data-action="prev">«</span>
+        <span class="active">{{this.currentPage}}</span>
+        <div class="pages" v-show="(this.currentPage+this.default) < pageCount">
+            <span data-action="next">{{this.currentPage+this.default}}</span>
+            <span>...</span>
         </div>
-        <span class="next">»</span>
+        <span v-show="this.currentPage !== pageCount" data-action="last">{{pageCount}}</span>
+        <span class="next" data-action="next">»</span>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "Pagination"
+import { mapState } from 'vuex'
+export default {
+  name: 'Pagination',
+  data () {
+    return {
+      page: 1
     }
+  },
+  computed: {
+    ...mapState({
+      currentPage: 'currentPage'
+    }),
+    pageCount () {
+      return this.adds.length / this.limit
+    }
+  },
+  props: ['adds', 'limit', 'default'],
+  methods: {
+    changePage (e) {
+      let action = e.target.dataset.action
+
+      if (this.currentPage < this.pageCount && action === 'next') {
+        this.page = this.currentPage + this.default
+      } else if (this.currentPage !== this.default && action === 'prev') {
+        this.page = this.currentPage - this.default
+      } else if (this.currentPage < this.pageCount && action === 'last') {
+        this.page = this.pageCount
+      }
+
+      this.$store.dispatch('setPage', {data: this.page})
+    }
+  }
+}
 </script>
